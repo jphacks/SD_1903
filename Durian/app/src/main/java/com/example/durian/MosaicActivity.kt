10980b86a186821d4e439f13d1293b84ea1c65fb
Cyan_on_Mac <namid224y@gmail.com>
2019-10-26 09:43:10 +0900

    add four activities

---
+++ Durian/app/src/main/java/com/example/durian/MosaicActivity.kt
@@ -0,0 +1,12 @@
+package com.example.durian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MosaicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mosaic)
+    }
+}
---
+++ Durian/app/src/main/java/com/example/durian/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.durian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
